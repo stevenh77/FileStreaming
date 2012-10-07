40eee1b2aec50d7fd130a18492f3722ac7aee356
Steven Hollidge <stevenhollidge@hotmail.com>
2012-10-07 18:43:54 +0200

    Initial upload

---
+++ FileStreaming.Web/DownloadFile.ashx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+namespace FileStreaming.Web
+{
+    public class DownloadFile : IHttpHandler
+    {
+        private const string FilesDirectory = @"\Files\";
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.Request.QueryString == null || context.Request.QueryString["reportid"] == null)
+            {
+                return;
+            }
+
+            var reportId = int.Parse(context.Request.QueryString["reportid"]);
+
+            var filename = ConvertReportIdToFileName(reportId);
+
+            var fullPath = Path.Combine(HttpContext.Current.Server.MapPath(FilesDirectory), filename);
+
+            var contentType = GetContentType(filename);
+
+            using (var reader = new StreamReader(fullPath))
+            {
+                var result = reader.ReadToEnd();
+                context.Response.ContentType = contentType;
+                context.Response.AddHeader("content-disposition", string.Format("attachment; filename={0}", filename));
+                context.Response.Write(result);
+            }
+        }
+
+        private string ConvertReportIdToFileName(int reportId)
+        {
+            switch (reportId)
+            {
+                case 1:
+                    return @"report1.xlsx";
+                case 2:
+                    return @"report2.docx";
+                case 3:
+                    return @"report3.pdf";
+                default:
+                    throw new ArgumentException("Unknown ReportId");
+            }
+        }
+
+        private string GetContentType(string file)
+        {
+            var fileExtension = file.Substring(file.Length - 4, 4).Replace(".", "");
+
+            switch (fileExtension)
+            {
+                case "xlsx":
+                    return "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+                case "docx":
+                    return "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+
+                case "pdf":
+                    return "application/pdf";
+
+                case "json":
+                    return "text/json";
+
+                case "xml":
+                    return "text/xml";
+
+                default:
+                    return "unknown";
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ FileStreaming/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Browser;
+
+namespace FileStreaming
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReport1_Click(object sender, RoutedEventArgs e)
+        {
+            Get("DownloadFile.ashx?reportid=1");
+            //Get(@"Files/report1.xlsx");
+        }
+
+        private void btnReport2_Click(object sender, RoutedEventArgs e)
+        {
+            Get("DownloadFile.ashx?reportid=2");
+            //Get(@"Files/report2.docx");
+        }
+
+        private void btnReport3_Click(object sender, RoutedEventArgs e)
+        {
+            Get("DownloadFile.ashx?reportid=3");
+            //Get(@"Files/report3.pdf");
+        }
+
+        private void Get(string file)
+        {
+            HtmlPage.Window.Navigate(new Uri(file, UriKind.Relative), "_blank");
+        }
+    }
+}
